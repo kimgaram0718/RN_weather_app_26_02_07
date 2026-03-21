5002f812d7a12b98cecd1d6e8053601608368d34
kimgaram <kimgaram0718@naver.com>
2026-03-21 19:31:24 +0900

    위도와 경도를 기반으로 현재 주소 가져오기

--- App.js
+++ App.js
@@ -1,17 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 
 const {SCREEN_WINDOW} = Dimensions.get('window').width;
 console.log(SCREEN_WINDOW);
 
 const App= () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [city, setCity] = useState(null);
+  //허가여부
+  cont [permitted, setPermitted] = useState(true);
+  const locationData = async() => {
+    const granted = await Location.requestForegroundPermissionsAsync();
+    console.log(granted);
+    if (granted) {
+      setPermitted(false);
+      setErrorMsg('위치에 대한 권한 부여가 거부되었습니다.');
+      return;
+    }
+    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5});
+    // console.log(latitude); //위도
+    // console.log(longitude); //경도
+    
+    const address = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    // console.log(address);
+    // console.log(address[0].city);
+    const cityAddress = address[0].city;
+    setCity(cityAddress);
+  }
+
+  useEffect(() => {
+    locationData(address[0].city);
+  }, []);
   return (
     // flextDirection: "row" 하면 기존 빨,오,녹이 상>하였는데 좌>우로 됨 float처럼 둥둥 띄움
     <View style={styles.container}>
       <View style={styles.cityCon}>
-        <Text style={styles.city}>Ansan</Text>
+        <Text style={styles.city}>{city}</Text>
       </View>
       <View style={styles.regDateCon}>
         <Text style={styles.regDate}>10월 13일, 일, 13:18</Text>
